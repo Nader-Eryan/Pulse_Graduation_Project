41332ef5b3349061ca70a18b6c23e11ea7ae0ab4
Nader-Eryan <ne1309@fayoum.edu.eg>
2024-03-02 13:32:33 +0200

    disposed sign up controllers

--- lib/Features/authentication/presentation/views/registration_view.dart
+++ lib/Features/authentication/presentation/views/registration_view.dart
@@ -12,9 +12,6 @@ import 'package:pulse/Features/authentication/data/repo/auth_repo_impl.dart';
 import 'package:pulse/Features/authentication/presentation/views/signin_view.dart';
 
 class RegistrationView extends StatelessWidget {
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final _profileReop = ProfileRepoImpl();
   RegistrationView({Key? key}) : super(key: key);
@@ -30,82 +27,84 @@ class RegistrationView extends StatelessWidget {
           padding: const EdgeInsets.all(kPaddingView),
           child: Form(
             key: _formKey,
-            child: Column(
-              children: <Widget>[
-                CustomFormField(
-                  isSuffixIcon: false,
-                  isPassWord: false,
-                  hintText: 'Enter your name',
-                  controller: nameController,
-                  prefixIcon: FontAwesomeIcons.user,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
-                      return 'Please enter a valid name';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFormField(
-                  isSuffixIcon: false,
-                  isPassWord: false,
-                  hintText: 'Enter your email',
-                  controller: emailController,
-                  prefixIcon: Icons.email_outlined,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!RegExp(
-                            r'''^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$''')
-                        .hasMatch(value)) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                CustomFormField(
-                  isSuffixIcon: true,
-                  isPassWord: true,
-                  hintText: 'Enter your password',
-                  controller: passwordController,
-                  prefixIcon: Icons.lock_outline,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    } else if (!RegExp(
-                            r'''^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,20}$''')
-                        .hasMatch(value)) {
-                      return '8+ length, 1+ (digit, lower, upper, special char)';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                AgreementCheckbox(
-                  profileRepo: _profileReop,
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                GetBuilder(
-                  init: SignUpController(),
-                  builder: (controller) => CustomMaterialButton(
+            child: GetBuilder(
+              init: SignUpController(),
+              builder: (controller) => Column(
+                children: <Widget>[
+                  CustomFormField(
+                    isSuffixIcon: false,
+                    isPassWord: false,
+                    hintText: 'Enter your name',
+                    controller: controller.nameController,
+                    prefixIcon: FontAwesomeIcons.user,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+                        return 'Please enter a valid name';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  CustomFormField(
+                    isSuffixIcon: false,
+                    isPassWord: false,
+                    hintText: 'Enter your email',
+                    controller: controller.emailController,
+                    prefixIcon: Icons.email_outlined,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      } else if (!RegExp(
+                              r'''^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$''')
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  CustomFormField(
+                    isSuffixIcon: true,
+                    isPassWord: true,
+                    hintText: 'Enter your password',
+                    controller: controller.passwordController,
+                    prefixIcon: Icons.lock_outline,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      } else if (!RegExp(
+                              r'''^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,20}$''')
+                          .hasMatch(value)) {
+                        return '8+ length, 1+ (digit, lower, upper, special char)';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  AgreementCheckbox(
+                    profileRepo: _profileReop,
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  CustomMaterialButton(
                     screenRatio: 0.9,
                     text: 'Sign Up',
                     onPressed: () {
                       if (_formKey.currentState!.validate() &&
                           controller.isChecked) {
-                        _profileReop.registerUser(context, emailController.text,
-                            passwordController.text);
+                        _profileReop.registerUser(
+                            context,
+                            controller.emailController.text,
+                            controller.passwordController.text);
                       }
                       if (_formKey.currentState!.validate() &&
                           !controller.isChecked) {
@@ -114,25 +113,25 @@ class RegistrationView extends StatelessWidget {
                       }
                     },
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Already have an account?",
-                        style: Styles.textStyleNormal14),
-                    TextButton(
-                      onPressed: () {
-                        Get.off(() => SignInView());
-                      },
-                      child: Text(
-                        'Sign In',
-                        style: Styles.textStyleSemiBold14
-                            .copyWith(color: const Color(0xFF407CE2)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?",
+                          style: Styles.textStyleNormal14),
+                      TextButton(
+                        onPressed: () {
+                          Get.off(() => SignInView());
+                        },
+                        child: Text(
+                          'Sign In',
+                          style: Styles.textStyleSemiBold14
+                              .copyWith(color: const Color(0xFF407CE2)),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
